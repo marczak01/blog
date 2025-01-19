56ed4c67c88a2353612683665964ca1f08d18777
Marczak01 <marczak01@icloud.com>
2025-01-19 19:42:08 +0100

    19.01.2025 19:41 powerfull search engine functionality with postgreSQL

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchRank, SearchQuery, TrigramSimilarity
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404, HttpResponse
 from blog.models import Post, Comment
@@ -183,7 +183,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            results = Post.published.annotate(search=SearchVector('title', 'body'), ).filter(search=query)
+            search_vector = SearchVector('title', weight='A') + SearchVector('body', weight='B')
+            search_query = SearchQuery(query, config='english')
+            results = Post.published.annotate(similarity=TrigramSimilarity('title', query), ).filter(similarity__gt=0.1).order_by('-similarity')
 
     context = {
         'form': form,
